d1178ac2d38ab7e4a322fd700d0ee0bdf98c77fb
or-mv <or.gamliel@mantis-vision.com>
2024-07-14 17:21:56 +0300

    modify kafka.ymal - now Kafka pod is now running succaessfully!

--- src/producer.py
+++ src/producer.py
@@ -1,21 +1,28 @@
 import time
 import requests
-from kafka import KafkaProducer
+from confluent_kafka import Producer
 import json
 
-producer = KafkaProducer(bootstrap_servers='kafka:9092',
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer_conf = {
+    'bootstrap.servers': 'kafka:9092',
+    'client.id': 'python-producer'
+}
+producer = Producer(producer_conf)
 
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Message delivery failed: {err}")
+    else:
+        print(f"Message delivered to {msg.topic()} [{msg.partition()}]")
 
 def fetch_and_publish():
     while True:
         response = requests.get('https://portal.thatapicompany.com/api/breeds')
         if response.status_code == 200:
             breeds = response.json()[:10]  # Get 10 random breeds
-            producer.send('breeds_topic', breeds)
+            producer.produce('breeds_topic', key=None, value=json.dumps(breeds), callback=delivery_report)
             producer.flush()
         time.sleep(60)
-
 
 if __name__ == "__main__":
     fetch_and_publish()

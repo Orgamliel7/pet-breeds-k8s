8fc2b66cf60c5c0e548b09e97b53c5cbe15c0909
Orgamliel7 <57177424+Orgamliel7@users.noreply.github.com>
2024-07-17 12:38:07 +0300

    Add files via upload
    project is working now without formatting errors. remove the old kafka-deployment file

--- consumer/main.py
+++ consumer/main.py
@@ -1,13 +1,16 @@
 import json
 import os
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer  # type: ignore
 
 KAFKA_TOPIC = 'pets'
-KAFKA_BROKER = os.getenv('KAFKA_SERVER', 'kafka-0.kafka:9092,kafka-1.kafka:9092,kafka-2.kafka:9092')
+KAFKA_SERVER = os.getenv(
+    'KAFKA_SERVER',
+    'kafka-0.kafka:9092,kafka-1.kafka:9092,kafka-2.kafka:9092'
+).split(',')
 
 consumer = KafkaConsumer(
     KAFKA_TOPIC,
-    bootstrap_servers=[KAFKA_BROKER],
+    bootstrap_servers=KAFKA_SERVER,
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
--- producer/main.py
+++ producer/main.py
@@ -1,8 +1,8 @@
-import requests
+import requests  # type: ignore
 import json
 import time
 import random
-from kafka import KafkaProducer
+from kafka import KafkaProducer  # type: ignore
 import os
 
 API_URLS = [
@@ -10,8 +10,12 @@ API_URLS = [
     "https://api.thecatapi.com/v1/breeds"
 ]
 
-KAFKA_BROKER = os.getenv('KAFKA_SERVER', 'kafka-0.kafka:9092,kafka-1.kafka:9092,kafka-2.kafka:9092')
+KAFKA_SERVER = os.getenv(
+    'KAFKA_SERVER',
+    'kafka-0.kafka:9092,kafka-1.kafka:9092,kafka-2.kafka:9092'
+).split(',')
 KAFKA_TOPIC = 'pets'
+
 
 def fetch_breeds():
     all_breeds = []
@@ -22,17 +26,24 @@ def fetch_breeds():
                 breeds = response.json()
                 all_breeds.extend(breeds)
             else:
-                print(f"Unexpected status code from {url}: {response.status_code}")
+                print(
+                    f"Unexpected status code from {url}: "
+                    f"{response.status_code}"
+                )
         except requests.exceptions.RequestException as e:
             print(f"Request failed for {url}: {e}")
     return all_breeds
 
+
 def get_random_breeds(breeds, n=10):
     return random.sample(breeds, min(n, len(breeds)))
 
+
 def main():
-    producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER],
-                             value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+    producer = KafkaProducer(
+        bootstrap_servers=KAFKA_SERVER,
+        value_serializer=lambda x: json.dumps(x).encode('utf-8')
+    )
 
     while True:
         all_breeds = fetch_breeds()
@@ -43,8 +54,8 @@ def main():
             print(f"Sent {len(random_breeds)} breeds to Kafka")
         else:
             print("No breeds fetched")
-        
         time.sleep(60)
+
 
 if __name__ == "__main__":
     main()
